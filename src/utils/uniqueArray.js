3d1184c960b201b8773b5d9599c799f0753266bb
guang <531311582@qq.com>
2024-05-18 11:51:31 +0800

    fix: 4

---
+++ src/utils/uniqueArray.js
@@ -0,0 +1,8 @@
+/**
+ * @author        guang <531311582@qq.com>
+ * @date          2024-05-18 11:50:14
+ * Copyright © YourCompanyName All rights reserved
+ */
+const uiqueArray = (arr) => {
+  return [...new Set(arr)];
+};
