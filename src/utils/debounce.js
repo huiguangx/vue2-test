9da3954dc32ddaa4544ea65b75435660656e00de
guang <531311582@qq.com>
2024-05-18 11:38:12 +0800

    fix: 2

---
+++ src/utils/debounce.js
@@ -0,0 +1,14 @@
+/**
+ * @author        guang <531311582@qq.com>
+ * @date          2024-05-18 11:35:05
+ * Copyright © YourCompanyName All rights reserved
+ */
+const debounce = (fn, delay) => {
+  let timer = null;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
